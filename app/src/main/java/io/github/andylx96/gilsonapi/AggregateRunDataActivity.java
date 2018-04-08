ae2206c253601e1364faf6915027f0654fe1810d
kcp5139 <kcp5139@psu.edu>
2018-04-08 14:17:59 -0400

    A Few Minor Changes

--- app/src/main/java/io/github/andylx96/gilsonapi/AggregateRunDataActivity.java
+++ app/src/main/java/io/github/andylx96/gilsonapi/AggregateRunDataActivity.java
@@ -11,4 +11,6 @@ public class AggregateRunDataActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aggregatedata);
     }
+
+
 }
