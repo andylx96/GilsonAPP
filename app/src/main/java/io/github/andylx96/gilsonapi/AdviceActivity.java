a89ff71bbda19473d85a0d63f474ca0b522e93e2
andylx96 <andylx96@users.noreply.github.com>
2018-04-09 16:55:16 -0400

    basic pop up screen

--- app/src/main/java/io/github/andylx96/gilsonapi/AdviceActivity.java
+++ app/src/main/java/io/github/andylx96/gilsonapi/AdviceActivity.java
@@ -2,6 +2,7 @@ package io.github.andylx96.gilsonapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 public class AdviceActivity extends AppCompatActivity {
 
@@ -10,7 +11,13 @@ public class AdviceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advice);
 
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
 
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*0.3),(int)(height*0.3));
 
     }
 }
