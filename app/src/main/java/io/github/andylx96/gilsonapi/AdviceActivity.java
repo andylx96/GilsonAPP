ae2cfa8f850ee435fa3eaee78d3375126c722093
rfd5085 <rfd5085@psu.edu>
2018-04-09 18:41:35 -0400

    Added more functionality

--- app/src/main/java/io/github/andylx96/gilsonapi/AdviceActivity.java
+++ app/src/main/java/io/github/andylx96/gilsonapi/AdviceActivity.java
@@ -4,12 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 
+import Snowboard.Snowboard;
+
 public class AdviceActivity extends AppCompatActivity {
+
+    Snowboard test = new Snowboard();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_advice);
+
+        double[] gyro = test.getGyroscope();
+        double x = gyro[0];
+
+        if(x < 0){setContentView(R.layout.activity_advice);}
+        if(x > 0){setContentView(R.layout.activity_advice2);}
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
