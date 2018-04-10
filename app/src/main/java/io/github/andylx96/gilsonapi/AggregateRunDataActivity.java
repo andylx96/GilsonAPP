dd2104ca7a7acc664afd027fcf9ecdca554661b0
Philip <pxk5145@psu.edu>
2018-04-10 02:56:51 -0400

    populating list view test 1

--- app/src/main/java/io/github/andylx96/gilsonapi/AggregateRunDataActivity.java
+++ app/src/main/java/io/github/andylx96/gilsonapi/AggregateRunDataActivity.java
@@ -4,6 +4,7 @@ package io.github.andylx96.gilsonapi;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -25,7 +26,10 @@ public class AggregateRunDataActivity extends AppCompatActivity{
         setContentView(R.layout.activity_aggregatedata);
 
         showRuns = findViewById(R.id.ShowRunsButton);
-        //list = findViewById(R.id.listView);
+        String [] myRuns = {"1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_aggregatedata, myRuns);
+        list = (ListView) findViewById(R.id.listView);
 
         showRuns.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,8 +43,6 @@ public class AggregateRunDataActivity extends AppCompatActivity{
 
             }
         });
-
-
 
     }
 
