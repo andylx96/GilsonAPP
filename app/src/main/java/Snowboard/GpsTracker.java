00e717b9777615dea8b298801ab907fb5b4ca61d
Logan <schlog2014@gmail.com>
2018-04-23 12:49:39 -0400

    Speed Added to Database

--- app/src/main/java/Snowboard/GpsTracker.java
+++ app/src/main/java/Snowboard/GpsTracker.java
@@ -15,17 +15,30 @@ import android.util.Log;
 
 import static android.content.Context.LOCATION_SERVICE;
 
-/**
- * Created by shiva on 8/4/17.
- */
+
 
 public class GpsTracker implements LocationListener {
 
     Context context;
+    double startLat;
+    double startLon;
+    double  endLat;
+    double endLon;
+    double runDist;
+    double runTime;
+    double speed;
+
 
     public GpsTracker(Context context) {
         super();
         this.context = context;
+        startLat = 0;
+        startLon = 0;
+        endLat = 0;
+        endLon = 0;
+        runDist = 0;
+        runTime = 0;
+        speed= 0;
     }
 
     public Location getLocation(){
@@ -37,7 +50,7 @@ public class GpsTracker implements LocationListener {
             LocationManager lm = (LocationManager) context.getSystemService(LOCATION_SERVICE);
             boolean isGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
             if (isGPSEnabled){
-                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000,10,this);
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000,1,this);
                 Location loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                 return loc;
             }else{
@@ -64,4 +77,52 @@ public class GpsTracker implements LocationListener {
     @Override
     public void onProviderDisabled(String provider) {
     }
+
+    //return length of run
+    public double getRunLength(){
+        //start lat
+        double theta = startLon - endLon;
+        runDist = Math.sin(deg2rad(startLat))
+                * Math.sin(deg2rad(endLat))
+                + Math.cos(deg2rad(startLat))
+                * Math.cos(deg2rad(endLat))
+                * Math.cos(deg2rad(theta));
+        runDist = Math.acos(runDist);
+        runDist = rad2deg(runDist);
+        runDist = runDist * 60 * 1.1515;
+        return (runDist);
+    }
+
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
+
+    public void setRunLength(double lat1, double lon1, double lat2, double lon2){
+        startLat = lat1;
+        startLon = lon1;
+        endLat = lat2;
+        endLon = lon2;
+    }
+
+    //return how long run lasted
+    public long getRunTime(){
+          return (long) runTime;
+    }
+
+    public void setRunTime(long time){
+        runTime = time;
+    }
+
+    public void setSpeed(double time, double dist ){
+        speed = dist/time;
+    }
+
+    public double getSpeed(){
+        return speed;
+    }
+
 }
